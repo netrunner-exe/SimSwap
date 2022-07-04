fa58546bb9be5fdb443074eb0f1e4986836381f7
netrunner-exe <81887288+netrunner-exe@users.noreply.github.com>
2022-07-04 15:01:20 +0300

    Update test_options.py

--- options/test_options.py
+++ options/test_options.py
@@ -8,6 +8,16 @@ Description:
 '''
 from .base_options import BaseOptions
 
+
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+
+
 class TestOptions(BaseOptions):
     def initialize(self):
         BaseOptions.initialize(self)
